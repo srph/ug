8c3abed07998125e904041ba30d535049b7156d8
srph <seraphipod@gmail.com>
2016-01-06 22:53:25 +0800

    Add job-to-be-called test.

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -2,14 +2,19 @@
 
 class ExampleTest extends TestCase
 {
+
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testBasicExample()
+    public function testJobToBeCalled()
     {
-        $this->visit('/')
-             ->see('Lumen.');
+        // https://github.com/laravel/lumen-framework/issues/207
+        unset($this->app->availableBindings['Illuminate\Contracts\Bus\Dispatcher']);  
+
+        $this->expectsJobs('App\Jobs\GenerateStylesheetArchive');
+        $this->post('/');
     }
+    
 }
