d71c33858687fe74f48db3bf26b7c78ad6841fc7
srph <seraphipod@gmail.com>
2016-01-06 22:12:20 +0800

    Fix public_path using sq.

--- app/helpers.php
+++ app/helpers.php
@@ -4,6 +4,6 @@ if ( !function_exists('public_path'))
 {
 	function public_path($path) {
 		$path = trim($path, '/');
-		return app()->basePath('public/{$path}');
+		return app()->basePath("public/{$path}");
 	}
 }
